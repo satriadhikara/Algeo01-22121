56c3367040bada815c74822a00a0bbbbcf366b1a
Petani Kode <contoh@petanikode.com>
2023-09-24 20:39:00 +0700

    operasi identitas

--- src/program/ADT/primitives/OperasiIdentitas.java
+++ src/program/ADT/primitives/OperasiIdentitas.java
@@ -2,10 +2,61 @@ package program.ADT.primitives;
 
 import program.ADT.Matrix;
 
-// Buat jojo
 
 public class OperasiIdentitas {
     public static boolean IsSquare(Matrix m) {
-        return true; // Contoh
+        if (m.row == m.col){
+            return true;
+        }else {
+            return false ;
+        }
     }
+
+    public static boolean IsSymetric(Matrix m) {
+        if (m.row != m.col){
+            return false;
+        }
+        int i, j;
+        for (i = 0; i < m.row; i++)
+        {
+            for (j = 0; j < m.col; j++)
+            {
+                if (m.elmt[i][j] != m.elmt[j][i])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean IsIdentity(Matrix m) {
+        if (m.row != m.col){
+            return false;
+        }
+        int i, j;
+        for (i = 0; i < m.row; i++)
+        {
+            for (j = 0; j < m.col; j++)
+            {
+                if (i == j)
+                {
+                    if (m.elmt[i][j] != 1)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (m.elmt[i][j] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
 }
