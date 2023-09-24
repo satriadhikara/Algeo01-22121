b38f1e44a61fb6c747823b698796fcf986b6b1fe
jojogantengbanget <jonathan.srgh@gmail.com>
2023-09-25 00:21:41 +0700

    Cramer

--- src/program/SPL/Cramer.java
+++ src/program/SPL/Cramer.java
@@ -1,23 +1,23 @@
 package program.SPL;
 
-import program.SPL.Gauss;
 import program.ADT.Matrix;
+import program.ADT.primitives.Determinan;
 
 public class Cramer {
-    public static int Penjumlahan(Matrix m) {
+    public static Matrix cramer(Matrix m) {
         int i, j;
+        Matrix cramer = Matrix.createMatrix(m.row, 1);
+        float D = Determinan.Determinant(m);
         for (i = 0; i < m.row; i++) {
-            Matrix m1 = Matrix.createMatrix(m.row, m.row);
+            Matrix m1 = Matrix.createMatrix(m.row, m.row); // Membuat matrix sementara untuk mencari determinan tiap
+                                                           // variabel
             for (j = 0; j < m.row; j++) {
-                if (j == 0) {
-                    m1.elmt[i][j] = m.elmt[i][m.col - 1];
-                } else {
-                    m1.elmt[i][j] = m.elmt[i][j];
-                }
+                m1.elmt[j][i] = m.elmt[j][m.col - 1];
             }
-            int det;
 
+            cramer.elmt[i][0] = Determinan.Determinant(m1) / D;
         }
+        return cramer;
 
     }
 
