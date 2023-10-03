40d39e135d821b04fbfa915ca2dd2d79fae30046
jojogantengbanget <jonathan.srgh@gmail.com>
2023-10-03 21:50:50 +0700

    revisi bicubic spline

--- src/program/Interpolasi/Bicubic.java
+++ src/program/Interpolasi/Bicubic.java
@@ -1,19 +1,30 @@
 package program.Interpolasi;
 
+import program.ADT.Matrix;
+
 public class Bicubic {
     //
-    public static double bicubic(double[][] p, double x, double y) {
+    public static double bicubic(Matrix p, double x, double y) {
 
-        double[] arr = new double[4];
+        Matrix arr = Matrix.createMatrix(1, 4);
 
         // Proses hitung interpolasi kubik pada setiap baris
         for (int i = 0; i < 4; i++) {
 
-            arr[i] = p[i][1] + 0.5 * x * (p[i][2] - p[i][0] + x * (2.0 * p[i][0] - 5.0 * p[i][1] + 4.0 * p[i][2]
-                    - p[i][3] + x * (3.0 * (p[i][1] - p[i][2]) + p[i][3] - p[i][0])));
+            arr.elmt[0][i] = p.elmt[i][1]
+                    + 0.5 * x
+                            * (p.elmt[i][2] - p.elmt[i][0]
+                                    + x * (2.0 * p.elmt[i][0] - 5.0 * p.elmt[i][1] + 4.0 * p.elmt[i][2]
+                                            - p.elmt[i][3]
+                                            + x * (3.0 * (p.elmt[i][1] - p.elmt[i][2]) + p.elmt[i][3] - p.elmt[i][0])));
         }
-        return arr[1] + 0.5 * y * (arr[2] - arr[0] + y * (2.0 * arr[0] - 5.0 * arr[1] + 4.0 * arr[2] - arr[3]
-                + y * (3.0 * (arr[1] - arr[2]) + arr[3] - arr[0])));
+        return arr.elmt[0][1]
+                + 0.5 * y
+                        * (arr.elmt[0][2] - arr.elmt[0][0]
+                                + y * (2.0 * arr.elmt[0][0] - 5.0 * arr.elmt[0][1] + 4.0 * arr.elmt[0][2]
+                                        - arr.elmt[0][3]
+                                        + y * (3.0 * (arr.elmt[0][1] - arr.elmt[0][2]) + arr.elmt[0][3]
+                                                - arr.elmt[0][0])));
 
     }
 }
